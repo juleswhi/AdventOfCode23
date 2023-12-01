357bc1fea130e1c509e2873474ae08cf892bbf23
jules white <62078259+juleswhi@users.noreply.github.com>
2023-12-01 14:47:59 +0000

    Day One

---
+++ DayOne/PuzzleOne/Program.cs
@@ -0,0 +1,19 @@
+﻿int runningTotal = 0;
+string[] inp = File.ReadAllLines("input.txt");
+
+List<int> nums = new();
+for (int i = 0; i < inp.Length; i++)
+{
+    nums.Clear();
+    for (int j = 0; j < inp[i].Length; j++)
+    {
+        if (int.TryParse($"{inp[i][j]}", out int num))
+        {
+            nums.Add(num);
+        }
+    }
+    int.TryParse($"{nums[0]}{nums[^1]}", out int final);
+    runningTotal += final;
+}
+
+Console.WriteLine(runningTotal);
