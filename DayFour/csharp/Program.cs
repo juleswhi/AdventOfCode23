38986bd97fe4ccb81b785daa19c907bf8d0538c6
jules white <62078259+juleswhi@users.noreply.github.com>
2023-12-04 20:31:59 +0000

    Day Four

---
+++ DayFour/csharp/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Schema;
+
+string[] inp = File.ReadAllLines("input");
+
+// Read in all the scratch cards
+foreach (var line in inp)
+{
+    var cards = line.Split(": ")[1];
+    var winning = cards.Split(" | ")[0].Split(" ").ToList();
+    winning.ForEach(x => x.ToString().Trim());
+    winning.Select(x => int.Parse($"{x}"));
+
+    var hand = cards.Split(" | ")[1].Split(" ").ToList();
+    hand.ForEach(x => x.ToString().Trim());
+    hand.Select(x => int.Parse($"{x}"));
+
+    string index = "";
+
+    foreach (var car in line.Split(": ")[0])
+    {
+        if (char.IsDigit(car))
+        {
+            index += car;
+        }
+    }
+
+    int currentIndex = int.Parse(index);
+
+
+
+
+    for (int i = 0; i < hand.Count; i++)
+    {
+        if (hand[i] == "") hand.Remove(hand[i]);
+    }
+
+    for (int i = 0; i < winning.Count; i++)
+    {
+        if (winning[i] == "") winning.Remove(winning[i]);
+    }
+
+    ScratchCard.Cards.Add(new ScratchCard(currentIndex, winning, hand) { Num = 1 });
+
+}
+
+foreach (var card in ScratchCard.Cards)
+{
+    int total = 0;
+    foreach (var win in card.Winning)
+    {
+        foreach (var hand in card.Hand)
+        {
+            if (win != hand) continue;
+            total += 1;
+            break;
+        }
+    }
+
+    for (int i = 1; i < total + 1; i++)
+    {
+        ScratchCard.AddCard(card.Index + i, card.Num);
+    }
+
+    Console.WriteLine("Card {0} Done, It generated, {1} cards", card.Index, card.Num * total);
+
+}
+
+
+
+foreach (var card in ScratchCard.Cards)
+{
+    Console.WriteLine(card.Index + ", " + card.Num);
+}
+
+Console.WriteLine($"Sum: {ScratchCard.Cards.Sum(x => x.Num)}");
+
+
+
+
+
+
+class ScratchCard
+{
+    public ScratchCard(int Index, List<string> Winning, List<string> Hand)
+    {
+        this.Index = Index;
+        this.Winning = Winning;
+        this.Hand = Hand;
+    }
+
+    public static List<ScratchCard> Cards { get; set; } = new();
+    public int Index { get; set; }
+    public int Num { get; set; } = 1;
+    public List<string> Winning { get; set; }
+    public List<string> Hand { get; set; }
+    public static void AddCard(int index, int times)
+    {
+        Cards[index - 1].Num += times;
+    }
+}
--- DayThree/rust/src/main.rs
+++ DayThree/rust/src/main.rs
@@ -3,13 +3,62 @@ fn main() {
 
     if args.len() != 2 {
         println!("Usage: cargo run -- <filename>");
+        return;
     }
 
     let file = std::fs::read_to_string(&args[1]).unwrap();
 
-    let lines = file.split("\n");
+    let lines = file.split("\n").collect::<Vec<&str>>();
 
-    for i in lines.enumerate() {
-        for j in i.1.chars().enumerate() {}
+    let mut i: isize = 0;
+    let mut j: isize;
+    let mut nums: Vec<isize> = Vec::new();
+
+    loop {
+        if i as usize >= lines.len() {
+            break;
+        }
+
+        nums.clear();
+        j = 0;
+
+        loop {
+            if j >= lines[i].chars().count() {
+                break;
+            }
+
+            for x in vec![-1, 0, 1] {
+                for y in vec![-1, 0, 1] {
+                    if (check_direction(&lines, (i + x) as isize, (j + y) as isize)) {
+                        nums.push(get_num(&lines, (i + x) as isize, (j + y) as isize));
+                    }
+                }
+            }
+
+            // Distinct from nums
+
+            j += 1;
+        }
+        i += 1;
     }
+
+    for num in nums {
+        println!("{num}");
+    }
+}
+
+fn check_direction(lines: &Vec<&str>, x: isize, y: isize) -> bool {
+    if lines[x as usize]
+        .chars()
+        .nth(y as usize)
+        .unwrap()
+        .is_digit(10)
+    {
+        return true;
+    }
+    false
+}
+
+fn get_num(lines: &Vec<&str>, x: isize, y: isize) -> isize {
+    lines[x as usize].chars().nth(y as usize).unwrap() as isize
 }
